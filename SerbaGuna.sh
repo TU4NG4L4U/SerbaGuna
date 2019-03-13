b832b4a9e3d133b9fe9163945eed26dc1f5ca8b7
TU4NG4L4U <46773642+TU4NG4L4U@users.noreply.github.com>
2019-03-14 06:18:14 +0700

    Add files via upload

---
+++ SerbaGuna.sh
@@ -0,0 +1,164 @@
+#Jangan Di Recode
+#Masa Gini Aja Gk Bisa :v
+#Mikir Pake Otak Punya Otak Juga
+
+clear
+echo
+echo
+echo
+echo
+figlet -f slant I . C . T | lolcat
+echo
+echo
+echo "                      |================================================| " | lolcat
+echo "                      |            -INDONESIAN  CYBER  TEAM-           | " | lolcat
+echo "                      |================================================| " | lolcat
+echo
+echo
+echo "                      |================================================| " | lolcat
+echo "                      \                                                / " | lolcat
+echo "                       |                 -Tools Serba Guna-            | " | lolcat
+echo "                       _______________________________________________" | lolcat
+echo
+echo "                       |---------------------------------------------|" | lolcat
+echo "                       | 1.Hack FB                                   |" | lolcat
+echo "                       | 2.CloningYahoo                              |" | lolcat
+echo "                       | 3.FB Crack                                  |" | lolcat
+echo "                       | 4.Buat Sc Deface                            |" | lolcat
+echo "                       | 5.Installer TU4N G4L4U                      |" | lolcat
+echo "                       | 6.Installer Sk4ters                         |" |lolcat
+echo "                       | 7.Hack Wifi (Root)                          |" | lolcat
+echo "                       | 8.main Moon-buggy (Game)                    |" | lolcat
+echo "                       | 9.LITE DDOS                                 |" | lolcat
+echo "                       | 10.B4J1N64N                                 |" | lolcat
+echo "                       -----------------|         |-------------------" | lolcat
+echo "                                        |         |"  | lolcat
+echo "            ____________________________-----------________________________________," | lolcat
+read -p "r00t@TU4NG4L4U|~# " pilih
+if [ $pilih = 1 ]
+then
+clear
+figlet -f slant W A I T | lolcay
+echo
+sleep 1
+git clone https://github.com/pirmansx/mbf
+cd mbf
+python2 MBF.py
+fi
+
+if [ $pilih = 2 ]
+then
+clear
+sleep 1
+pkg install python2
+pip install requests
+pip install mechanize
+pkg install git
+git clone https://github.com/wahyuandhika/YahooCloning
+cd YahooCloning
+python2 cloning.py
+fi
+
+if [ $pilih = 3 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo
+echo "Loading...!!"
+echo ""
+sleep 1
+apt install git
+pkg install python2
+pip2 install mechanize
+git clone https://github.com/rickyricko302/fb-crackv1
+cd fb-crackv1
+python2 fb-crack.py
+fi
+
+if [ $pilih = 4 ]
+then
+clear
+figlet -f slant TU4N G4L4U | lolcat
+echo
+echo "Loading........!!!!"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/4L13199/LITESCRIPT
+cd LITESCRIPT
+python2 LITESCRIPT.py
+fi
+
+if [ $pilih = 5 ]
+then
+ckear
+sleep 1
+gem install lolcat
+pkg install git
+git clone https://github.com/TU4NG4L4U/InstallerV6
+cd InstallerV6
+sh InstallerV6.sh
+
+if [ $pilih = 6 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo "Loading....!!!"
+sleep 1
+pkg install git
+git clone https://github.com/Sk4ters38/SK4T3R5v1
+cd SK4T3R5v1
+sh SK4T3RS.sh
+fi
+
+if [ $pilih = 7 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo "Loading....!!!"
+echo ""
+sleep 1
+pkg install git
+git clone https://github.com/derv82/wifite
+cd wifite
+chmod 777 wifite.py
+python2 wifite.py
+./wifite.py
+fi
+
+if [ $pilih = 8 ]
+then
+clear
+sleep 1
+pkg install moon-buggy
+moon-buggy
+fi
+
+if [ $pilih = 9 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo "\033[34;1mLoading.....!!!"
+echo ""
+sleep 1
+apt update && apt upgrade
+pkg install git
+pkg install python2
+git clone https://github.com/4L13199/LITEDDOS
+cd LITEDDOS
+python2 LITEDDOS.py
+fi
+
+if [ $pilih = 10 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo "\033[34;1mLoading......!!!"
+echo ""
+sleep 1
+apt update && apt upgrade
+pkg install git
+git clone https://github.com/DarknessCyberTeam/B4J1N64Nv5
+cd B4J1N64Nv5
+sh B4J1N64N.sh
+fi
